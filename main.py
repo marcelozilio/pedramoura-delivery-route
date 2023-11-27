f673bb3597c8ab5b48d84946c26211b62d677192
Yuri Arbeletche <ysba000@gmail.com>
2023-11-27 21:42:03 +0000

    fix flask instance to run in external ip

--- main.py
+++ main.py
@@ -45,4 +45,4 @@ api.add_resource(DeliveryRoute, '/delivery-route')
 api.add_resource(DeliveryRouteById, '/delivery-route/<_id>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
